7e5d464be1b5190f7f92e592545e2ca7d66776df
Thomas Kujawa <webmaster@familienfreund.de>
2024-01-14 16:09:48 +0100

    Update index.php to 2024
    Änderung Jahreszahl und Domain

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require ('data.php');
 
-$year = 2019;
+$year = 2024;
 $title = 'Adventskalender '.$year;
 
 $now = time();
@@ -37,7 +37,7 @@ endfor; ?>
     <script><?php include('js.php') ?></script>
   </head>
   <body>
-    <h1><a href="https://selfhtml.org"><?=$title?></a></h1>
+    <h1><a href="https://advent.radelei.de"><?=$title?></a></h1>
     <ol> <?php 
       for ($k = 0; $k < sizeof($listitems); $k++) : ?>
         <li><?=$listitems[$k]?></li> <?php
